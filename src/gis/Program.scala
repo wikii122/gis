3cea4830d9beb0ca3a6ad43ed5a7b8978b1a7157
Wiktor <wikii122@gmail.com>
2016-04-07 13:24:14 +0200

    Initialized repo
    Added hello world program skeleton

---
+++ src/gis/Program.scala
@@ -0,0 +1,13 @@
+package gis
+
+/**
+  * Program launcher.
+  *
+  * TODO: usage information
+  * TODO: help message
+  */
+object Program {
+  def main(args: Array[String]) = {
+    println("Hello world")
+  }
+}
